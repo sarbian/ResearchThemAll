a075e17a2e2280e88a1350d23a67484f5592e3ed
Sarbian <Sarbian@sarbian.com>
2014-10-16 20:48:36 +0200

    0.25 fix

--- ResearchThemAll.cs
+++ ResearchThemAll.cs
@@ -1,13 +1,9 @@
 ﻿/*
  * Copyright (c) <2014>, Sébastien GAGGINI AKA Sarbian, France
  * License: Attribution 4.0 International (CC BY 4.0): http://creativecommons.org/licenses/by/4.0/
- *  
+ *
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 namespace ResearchThemAll
@@ -15,27 +11,56 @@ namespace ResearchThemAll
     [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
     public class ResearchThemAll : MonoBehaviour
     {
-
-        bool isRnDOpen = false;
+        private bool isRnDOpen = false;
+        private int unlockable = 0;
+        private int cost = 0;
 
         private void Start()
         {
-            GameEvents.onGUIRnDComplexSpawn.Add(new EventVoid.OnEvent(this.OnGUIRnDComplexSpawn));
-            GameEvents.onGUIRnDComplexDespawn.Add(new EventVoid.OnEvent(this.onGUIRnDComplexDespawn));
+            GameEvents.onGUIRnDComplexSpawn.Add(OnGUIRnDComplexSpawn);
+            GameEvents.onGUIRnDComplexDespawn.Add(OnGUIRnDComplexDespawn);
+            GameEvents.OnTechnologyResearched.Add(OnTechnologyResearched);
         }
 
         private void OnDestroy()
         {
-            GameEvents.onGUIRnDComplexSpawn.Remove(new EventVoid.OnEvent(this.OnGUIRnDComplexSpawn));
-            GameEvents.onGUIRnDComplexDespawn.Remove(new EventVoid.OnEvent(this.onGUIRnDComplexDespawn));
+            GameEvents.onGUIRnDComplexSpawn.Remove(OnGUIRnDComplexSpawn);
+            GameEvents.onGUIRnDComplexDespawn.Remove(OnGUIRnDComplexDespawn);
+            GameEvents.OnTechnologyResearched.Remove(OnTechnologyResearched);
+        }
+
+        private void OnTechnologyResearched(GameEvents.HostTargetAction<RDTech, RDTech.OperationResult> data)
+        {
+            OnGUIRnDComplexSpawn();
         }
 
         private void OnGUIRnDComplexSpawn()
         {
             isRnDOpen = (ResearchAndDevelopment.Instance != null);
+
+            if (!isRnDOpen)
+            {
+                return;
+            }
+
+            foreach (RDNode node in FindObjectsOfType(typeof(RDNode)))
+            {
+                if (node.tech != null && node.IsResearched)
+                {
+                    for (int i = 0; i < node.tech.partsAssigned.Count; i++)
+                    {
+                        AvailablePart item = node.tech.partsAssigned[i];
+                        if (item != null && !node.tech.partsPurchased.Contains(item))
+                        {
+                            unlockable++;
+                            cost += item.entryCost;
+                        }
+                    }
+                }
+            }
         }
 
-        private void onGUIRnDComplexDespawn()
+        private void OnGUIRnDComplexDespawn()
         {
             isRnDOpen = false;
         }
@@ -43,54 +68,60 @@ namespace ResearchThemAll
         private void OnGUI()
         {
             if (!isRnDOpen)
-                return;
-
-            int unlockable = 0;
-
-            foreach (RDNode node in UnityEngine.Object.FindObjectsOfType(typeof(RDNode)))
             {
-                if (node.IsResearched)
-                    unlockable += node.PartsNotUnlocked();
+                return;
             }
 
+            bool enoughFunds = Funding.Instance == null || Funding.Instance.Funds >= cost;
+
             if (unlockable > 0)
-                PopupDialog.SpawnPopupDialog(new MultiOptionDialog("There are " + unlockable + " part to research. Do you want to research them all ?", "Research Them All", HighLogic.Skin,
-                    new DialogOption[] { new DialogOption("Research Them All", new Callback(this.OnResearchAllConfirm)), new DialogOption("Cancel", new Callback(this.OnResearchAllCancel)) }), false, HighLogic.Skin);
-            else
-                isRnDOpen = false;
-        }
-
-
-        private void OnResearchAllConfirm()
-        {
-
-            foreach (RDNode node in UnityEngine.Object.FindObjectsOfType(typeof(RDNode)))
             {
-                if (node.IsResearched)
+                if (enoughFunds)
                 {
-                    for (int i = 0; i < node.tech.partsAssigned.Count; i++)
-                    {
-                        AvailablePart item = node.tech.partsAssigned[i];
-                        if (!node.tech.partsPurchased.Contains(item))
-                        {
-                           node.tech.PurchasePart(item);
-                        }
-                    }
-                    node.graphics.SetAvailablePartsCircle(node.PartsNotUnlocked());
+                    string question1 = "There are " + unlockable +
+                                       " part to research. Do you want to research them all";
+                    string question2 = Funding.Instance != null
+                        ? " for " + cost.ToString("N0") + " Funds ?"
+                        : " ?";
+
+                    OnGUIRnDComplexDespawn();
+                    PopupDialog.SpawnPopupDialog(
+                        new MultiOptionDialog(question1 + question2
+                            , "Research Them All", HighLogic.Skin,
+                            new[]
+                            {
+                                new DialogOption("Research Them All", OnResearchAllConfirm),
+                                new DialogOption("Cancel", OnGUIRnDComplexDespawn)
+                            }),
+                        false,
+                        HighLogic.Skin);
+                }
+                else
+                {
+                    OnGUIRnDComplexDespawn();
+                    PopupDialog.SpawnPopupDialog("Research Them All",
+                        "Not enough Funds to unlock the " + unlockable + " part to research.", "Cancel", false,
+                        HighLogic.Skin);
                 }
             }
         }
 
-        private void OnResearchAllCancel()
+        private void OnResearchAllConfirm()
         {
-            isRnDOpen = false;
+            foreach (RDNode node in FindObjectsOfType(typeof(RDNode)))
+            {
+                if (node.tech != null && node.IsResearched)
+                {
+                    node.tech.AutoPurchaseAllParts();
+                    node.graphics.SetAvailablePartsCircle(node.PartsNotUnlocked());
+                }
+            }
+            OnGUIRnDComplexDespawn();
         }
-
 
         public new static void print(object message)
         {
             MonoBehaviour.print("[ResearchThemAll] " + message);
         }
-
     }
 }
